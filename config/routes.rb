13c030fc9e54e7527a79a88a7009b6bda21b327e
Benjamin Teste <benjamin.teste@gmail.com>
2020-04-30 16:32:55 +0200

    challenge done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  root "tasks#index"
-  get "tasks", to: "tasks#index"
+  root 'tasks#index'
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
